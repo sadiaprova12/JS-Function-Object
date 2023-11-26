6ab940d51da21ae323c8e493301ad4804e45a3fa
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-26 21:51:25 +0600

    added return

--- return.js
+++ return.js
@@ -2,7 +2,11 @@ function add(number1, number2){
     console.log(number1, number2);
     var sum = number1 + number2;
     console.log(sum);
-    // return sum;
+    // variable return
+    return sum;
 } 
 
-add(45, 15);
+// add(45, 15);
+
+var total = add(80, 20);
+console.log('total', total);
