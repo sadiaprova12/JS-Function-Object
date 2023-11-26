5bb93fa335a3e904a0d69ddbec87e07e5268c025
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-26 21:39:29 +0600

    added sum parameters

--- parameter.js
+++ parameter.js
@@ -33,4 +33,11 @@ function add(num1,num2){
     console.log('Going to add:', num1, num2)
 }
 
-add(125, 96);
+// add(125, 96);
+
+function sum(a,b,c,d,e){
+    console.log(a,b,c,d,e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(15,98,56,20,50);
