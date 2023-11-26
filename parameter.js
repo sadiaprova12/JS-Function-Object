6ab2081f8d9ef3d8a14b151c228238644ed70c46
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-26 18:02:47 +0600

    added new function

--- parameter.js
+++ parameter.js
@@ -1,14 +1,22 @@
-// Declare a function
-function bringSingara(){
-    console.log('Mama please give me a singara');
-}
-// call the function
-//bringSingara();
+// // Declare a function
+// function bringSingara(){
+//     console.log('Mama please give me a singara');
+// }
+// // call the function
+// //bringSingara();
 
-// Declare a function
+// // Declare a function
+// function bringSingara(money){
+//     console.log('Mama please give me a singara');
+//     console.log(money);
+// }
+// // call the function
+// bringSingara(100);
+
+// function declare
 function bringSingara(money){
-    console.log('Mama please give me a singara');
-    console.log(money);
+    console.log('Give me a Singara');
+    console.log('How much? Tk:',money);
 }
-// call the function
+
 bringSingara(100);
