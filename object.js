a25f3fd89eda9d0cdb884c2d0e3f1c0c46a4ccfa
sadiaprova12 <sadiaprova51717@gmail.com>
2023-11-27 23:09:37 +0600

    added obiject

---
+++ object.js
@@ -0,0 +1,24 @@
+// Object hocche ekta variable . ja onk maan dharon korte pare.
+var student = { id: 115, 
+    name: 'Solamin Khan', 
+    class: 9, 
+    marks: 98
+}
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    stroage: '64gb',
+    camera: '7MP'
+}
+
+
+var myComputer = {
+     brand: 'hp',
+     price: 72000,
+     color: 'silver',
+     processor: 'i5'
+}
+
+// console.log(myComputer.price);
+console.log(myComputer.brand);
