53953db3c54377f9d7af97eca19b7825d12e66d7
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-03 20:46:25 +0600

    added property name

--- object-looping.js
+++ object-looping.js
@@ -27,5 +27,9 @@ console.log(values);
 
 // var keys = [ 'books', 'sunglass', 'keyboard', 'mouse', 'pen', 'shoes' ]
 for (var i = 0; i < keys.length; i++){
-    console.log(keys[i])
+    // console.log(keys[i])
+    var propertyName = keys[i];
+    var propertyValue = shoppingCart[propertyName];
+    console.log(propertyName);
 }
+
