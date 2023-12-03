c6def1d167570c73d8fd920c29299bc03190f395
sadiaprova12 <sadiaprova51717@gmail.com>
2023-12-03 20:41:59 +0600

    added var keys

--- object-looping.js
+++ object-looping.js
@@ -26,6 +26,6 @@ const values = Object.values(shoppingCart);
 console.log(values);
 
 // var keys = [ 'books', 'sunglass', 'keyboard', 'mouse', 'pen', 'shoes' ]
-for (var i = 0; i > keys.length; i++){
+for (var i = 0; i < keys.length; i++){
     console.log(keys[i])
 }
